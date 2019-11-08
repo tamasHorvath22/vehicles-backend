1fb950d59a7c6d280ea30d0436590297d2885f05
Horváth Tamás <horvatht@itware.hu>
2019-11-08 13:16:43 +0100

    update route added

--- api/vehicleApi.js
+++ api/vehicleApi.js
@@ -7,12 +7,12 @@ module.exports = function(app) {
     app.post('/vehicle', jsonParser, function (req, res) {
         let timestamp = new Date().getTime();
         let vehicle = Vehicle({
-            make: req.body.make,
-            type: req.body.type,
-            distance: req.body.distance,
-            workHours: req.body.workHours,
+            make: req.body.make || null,
+            type: req.body.type || null,
+            distance: req.body.distance || 0,
+            workHours: req.body.workHours || null,
             savedAt: timestamp,
-            modifiedAt: timestamp
+            modifiedAt: 0
         });
 
         vehicle.save(function(err) {
@@ -21,10 +21,34 @@ module.exports = function(app) {
         });
     });
 
+    app.put('/vehicle', jsonParser, function (req, res) {
+        let hasChanged = false;
+        Vehicle.findById(req.body.id, function (err, doc){
+            if (err) throw err;
+            Object.keys(req.body.data).forEach(key => {
+                if (doc[key]) {
+                    doc[key] = req.body.data[key];
+                    hasChanged = true;
+                }
+            })
+            if (hasChanged) {
+                doc.__v++;
+                doc.modifiedAt = new Date().getTime();
+                doc.save();
+                res.json(JSON.stringify(doc));
+            } else {
+                res.send('no changes!');
+            }
+          });
+    });
+
     app.get('/vehicle/:id', function(req, res) {
         let id = req.params.id;
         Vehicle.findById(id, function(err, vehicle) {
-            if (err) throw err;
+            if (err) {
+                res.send('there were some error!');
+                throw err;
+            }
             res.json(JSON.stringify(vehicle));
         });
     });
